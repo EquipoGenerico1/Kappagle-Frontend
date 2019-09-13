aa019c9ab5ac0aab5b222201fde817d3f875914b
marcosjsp <marcos.academia.2@gmail.com>
2019-09-13 18:13:33 +0100

    home,login & signup views and components

---
+++ src/views/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="signUp">
+      signUp
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'signUp',
+  components: {
+  }
+}
+</script>
