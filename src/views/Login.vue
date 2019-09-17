cf46be981e6f2c7d5ae7bdc272fc3387c1c61f4b
Gabriel <g48r132@gmail.com>
2019-09-17 16:43:02 +0100

    Added login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,61 @@
 <template>
   <div class="login">
-      login
+    <div class="user-pass">
+        <div class="login-email">
+          <label>email</label>
+          <input type="text" name ="email" v-model="email">
+        </div>
+        <div class="login-pass">
+          <label>Password</label>
+          <input type="text" name ="password" v-model="password">
+        </div>
+    </div>
+      <div class="button-login">
+        <KButton class="button" value="login" @click="guacamole"></KButton>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import KButton from "../components/Button";
+import axios from "axios";
 
 export default {
   name: 'login',
   components: {
+    KButton
+  },
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    guacamole: function() {
+      axios.post("http://localhost:5000/api/v1/login", {
+        email: this.email,
+        password: this.password
+      }).then(res => {
+        localStorage.setItem("token", JSON.stringify(res.data));
+      }).catch(error => {
+        console.log(error);
+      });
+
+    }
   }
 }
 </script>
+
+<style scoped>
+.button {
+  flex-basis: 100%;
+  height: 10vh;
+  width: calc(100% - 10px);
+  position: fixed;
+  left: 5px;
+  bottom: 5px;
+}
+
+</style>
