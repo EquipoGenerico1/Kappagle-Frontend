cd219d406b043fa094f1b775061e9e57a4bf7d78
Gabriel <g48r132@gmail.com>
2019-09-18 14:25:07 +0100

    Added style and redirect

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="login">
     <div class="user-pass">
+      <h1>Login</h1>
         <div class="login-email">
-          <label>email</label>
-          <input type="text" name ="email" v-model="email">
+          <label>Email</label>
+          <div class="input email">
+            <input type="text" name ="email" v-model="email">
+          </div>
         </div>
         <div class="login-pass">
           <label>Password</label>
-          <input type="text" name ="password" v-model="password">
+          <div class="input pass">
+            <input type="password" name ="password" v-model="password">
+          </div>
         </div>
     </div>
-      <div class="button-login">
-        <KButton class="button" value="login" @click="login"></KButton>
-      </div>
+    <div class="button-login">
+      <KButton class="button" value="login" @click="login"></KButton>
+    </div>
   </div>
 </template>
 
@@ -61,5 +66,33 @@ export default {
   left: 5px;
   bottom: 5px;
 }
+/**/
+h1 {
+  font-weight: 700;
+  text-align: center;
+  line-height: 1.5em;
+  margin-bottom: 1.2em;
+  margin-top: 0.2em;
+}
+
+.login {
+  font-family: sans-serif;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  padding: 6%;
+  margin: 0;
+}
+.login-email {
+  margin-bottom: 1.2em;
+}
+label {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1em;
+}
+
+
 
 </style>
